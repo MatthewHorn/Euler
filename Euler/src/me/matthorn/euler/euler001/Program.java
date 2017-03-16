0dc058b574abf0b07a6ab06c6bb245c6f2124393
Matthew <matthew.horn@wmtps.net>
2017-03-16 16:16:17 -0400

    Remove answer in file.

--- Euler/src/me/matthorn/euler/euler001/Program.java
+++ Euler/src/me/matthorn/euler/euler001/Program.java
@@ -5,8 +5,6 @@ get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.*/
 
-// ANSWER: 233168
-
 public class Program
 {
 	public static void main(String[] args)
